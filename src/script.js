2180188f956336951418af84e020980f4e40f827
Alfrugo <diez_alfredo@hotmail.com>
2023-01-18 13:25:39 -0600

    changing canvas size

--- src/script.js
+++ src/script.js
@@ -14,8 +14,8 @@ const canvas = document.querySelector('canvas.webgl')
 // const canvasWidth = document.getElementById('canvas2').clientWidth
 // const canvasHeight = document.getElementById('canvas2').clientHeight
 
-const canvasWidth = 300 
-const canvasHeight = 300
+const canvasWidth = 600 
+const canvasHeight = 600
 
 
 
